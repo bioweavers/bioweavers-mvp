8b7f75c18a1e670ffab2a1d818186d36259db6a7
jaslynmiura <jaslynmiura@gmail.com>
2026-05-02 15:54:23 -0700

    Styling and navigation updates.

--- pages/1_Results.py
+++ pages/1_Results.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import time
 
 from src.species import plot_species_map_streamlit, refactor_cnps, plot_cnddb_species_distribution_streamlit, plot_cnddb_species_date_range
 
 
 st.title("Project Species Occurrences Results")
+
 
 if not st.session_state.get("results_ready"):
     st.info("Run a search on the main page first.")
@@ -15,23 +17,25 @@ search_area = st.session_state.search_area
 project_boundary_gdf = st.session_state.project_boundary_gdf
 
 # Map CNDDB species occurrences within the project boundary.
-st.header("CNDDB Species Map", divider=True)
+st.header("CNDDB Species Map", divider="grey")
 plot_species_map_streamlit(cnddb_species, search_area, project_boundary_gdf) 
 
-st.header("CNDDB Analysis Output", divider=True)
+st.header("CNDDB Analysis Output", divider='grey')
 
 # Graph the number of CNDDB species occurrences .
 st.subheader("CNDDB Species Occurrence")
 plot_cnddb_species_distribution_streamlit(cnddb_species)
 
-st.subheader("Species Occurrence Raw Data", divider=True)
+st.header("Species Occurrence Raw Data", divider='grey')
 
 # Display the results in tables.
-st.subheader("CNDDB Species Results")
+st.subheader("California Natural Diversity Database Species Results")
 st.write(f"Found {len(cnddb_species)} species occurrences")
 st.dataframe(cnddb_species.drop(columns='geometry'))
 
 # Display the results in tables.
-st.subheader("CNPS Species Results")
+st.subheader("California Native Plant Society Species Results")
 st.write(f"Found {len(cnps_species)} species occurrences")
 st.dataframe(cnps_species)
+
+st.page_link("pages/2_Table.py", label="Go to PTO Table Page", width='stretch')
